9623e8b313758ff6b8398fc77ca3682cb59a31e5
Jason De Souza <j.desouza206@gmail.com>
2023-08-02 04:47:41 +0100

    cleaned up, make walk time a param, added doc string and changed function name

--- code.py
+++ code.py
@@ -1,9 +1,17 @@
-print("WSD")
-import heapq
-def most_basic_possible(queue, flow_rates): # switch to individual tap
+
+def compute_fill_time(queue, flow_rates, walk_time=3):
+    """Computes the time needed for queue of water bottles to be filled by taps
+
+    Args:
+        queue (float): The volumes in ml of each bottle in the queue, index 0 represents first bottle in queue
+        flow_rates (float): The flow rate corresponding to each tap in ml/s
+        walk_time (float): Time taken for any person in queue to walk to tap in seconds (default is 3)
+
+    Returns:
+        list: a list of strings representing the header columns
+    """
 
     num_taps = len(flow_rates)
-    walk_time = 3 # time to walk to tap
     print("initial q:", queue)
 
     # assign first people to empty taps (intialise)
@@ -14,11 +22,14 @@ def most_basic_possible(queue, flow_rates): # switch to individual tap
 
     ## validation
     if any(map(lambda x : x < 0, queue)):
-        print(" ERROR MESSAGE INVALID BOTTLE VOLUMES")
+        raise Exception("Cannot have negative bottle volumes")
     if num_taps < 1:
-        print(" ERROR NO TAPS AVAILABLE")
+        raise Exception("Must have at least one tap")
     if any(map(lambda x : x < 0, flow_rates)):
-        print(" ERROR MESSAGE INVALID FLOW RATE/S")
+        raise Exception("Cannot have negative tap flow rates")
+    if walk_time < 0:
+        raise Exception("Cannot have negative time to walk to tap")
+
 
     q_idx = num_taps
     current_time = 0
@@ -60,26 +71,26 @@ q4 = [200, 200, 200, 100, 150, 100]
 walk_time = 3
 flow_rate = 100
 print("TEST1: ")
-assert(most_basic_possible(q1, [100] * 2) == 500/flow_rate + 2 * walk_time)
+assert(compute_fill_time(q1, [100] * 2) == 500/flow_rate + 2 * walk_time)
 print("TEST2: ")
-assert(most_basic_possible(q2, [100] * 3) == 600/flow_rate + 3 * walk_time)
+assert(compute_fill_time(q2, [100] * 3) == 600/flow_rate + 3 * walk_time)
 print("TEST3: ")
-assert(most_basic_possible(q3, [100] * 3) == 300/flow_rate + 2 * walk_time)
+assert(compute_fill_time(q3, [100] * 3) == 300/flow_rate + 2 * walk_time)
 print("TEST4: ")
-assert(most_basic_possible(q4, [100] * 3) == 350/flow_rate + 2 * walk_time)
+assert(compute_fill_time(q4, [100] * 3) == 350/flow_rate + 2 * walk_time)
 print("TEST5: vary rate")
-assert(most_basic_possible(q1, [50, 150]) == 200/50 + 200/50 + 2 * walk_time)
+assert(compute_fill_time(q1, [50, 150]) == 200/50 + 200/50 + 2 * walk_time)
 
 
 print("\npart 4 before:")
-most_basic_possible([100,10,1000], [100,5]) #t1 takes 1s (+3 for walk), t2 takes 2 seconds (+3 for walk), so t1 gets big task => result 4s+ 13s = 17
+compute_fill_time([100,10,1000], [100,5]) #t1 takes 1s (+3 for walk), t2 takes 2 seconds (+3 for walk), so t1 gets big task => result 4s+ 13s = 17
 print("\npart 4 after:")
-most_basic_possible([100,10,1000], [100,20]) # t1 takes 1s (+3 for walk), t2 takes half a second (+3 for walk), so t2 gets big task => 3.5s + 53s = 56.5s
+compute_fill_time([100,10,1000], [100,20]) # t1 takes 1s (+3 for walk), t2 takes half a second (+3 for walk), so t2 gets big task => 3.5s + 53s = 56.5s
 
 # THIS EXAMPLE USES 1000ML FOR FIRST TWO TASKS, SO EVEN IF SWITCHED AROUND WOULDNT CHANGE OUTCOME
 # the 3s in the calculations are for walking times. (You can scale down all the numbers if you scale down walking time too)
-print("\npart 5 better example before:")
-most_basic_possible([1000, 1000, 8000, 10000], [100,10]) # t1 takes 13s, t2 takes 103s, so t1 gets 8k and takes 83s which is 96s total so far so it then gets 10k which takes 103s totalling 199s
+print("\npart 4 better example before:")
+compute_fill_time([1000, 1000, 8000, 10000], [100,10]) # t1 takes 13s, t2 takes 103s, so t1 gets 8k and takes 83s which is 96s total so far so it then gets 10k which takes 103s totalling 199s
 
-print("\npart 5 better example after:")
-most_basic_possible([1000, 1000, 8000, 10000], [100,20]) # t1 takes 13s, t2 takes 53s, so t1 gets 8k and takes 83s which is 96 total so far so t2 gets 10k this time which takes 503 seconds totalling 556s 
+print("\npart 4 better example after:")
+compute_fill_time([1000, 1000, 8000, 10000], [100,20]) # t1 takes 13s, t2 takes 53s, so t1 gets 8k and takes 83s which is 96 total so far so t2 gets 10k this time which takes 503 seconds totalling 556s 
